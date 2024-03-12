c7ed86a4f624e5713826c18414ac5bf2d6940087
cozy_hn <jhk2721@gmail.com>
2024-03-12 16:09:30 +0900

    Update megaphone.cpp : Add ex00 all

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+#include <cctype>
 
-int main() {
-  std::cout << "Hello, World!!" << std::endl;
-  return 0;
+using std::cout;
+using std::string;
+using std::toupper;
+
+void print_upper(const string &str) {
+	for (size_t i = 0; i < str.length(); i++)
+		cout << (char)toupper(str[i]);
+}
+
+int main(int argc, char **argv) {
+	if (argc == 1)
+		print_upper("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+	else
+		for (int i = 1; i < argc; i++)
+			print_upper(argv[i]);
+	print_upper("\n");
 }
