6be80b4daad62940b033ea7ac952368c4e0c8a3c
cozy_hn <jhk2721@gmail.com>
2024-03-30 13:50:12 +0900

    Update project : set cpp02 base file

---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class fixed
+{
+public:
+	fixed();
+	fixed(const fixed& obj);
+	~fixed();
+	fixed&	operator=(const fixed& obj);
+
+private:
+
+};
+
+#endif
---
+++ CPP02/ex02/Fixed.cpp
@@ -0,0 +1,17 @@
+#include "fixed.hpp"
+
+fixed::fixed()
+{
+}
+
+fixed::fixed(const fixed& obj)
+{
+}
+
+fixed::~fixed()
+{
+}
+
+fixed&	fixed::operator=(const fixed& obj)
+{
+}
