ccb5f5e9ae9379d2a0dbec57b570687ebd27114c
cozy_hn <jhk2721@gmail.com>
2024-03-31 19:11:52 +0900

    Update CPP02/ex01 : update project

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -1,17 +1,63 @@
-#include "fixed.hpp"
+#include "Fixed.hpp"
 
-fixed::fixed()
+Fixed::Fixed(): value_(0)
 {
+	cout << "Default constructor called\n";
 }
 
-fixed::fixed(const fixed& obj)
+Fixed::Fixed(const Fixed& obj)
 {
+	cout << "Copy constructor called\n";
+	*this = obj;
 }
 
-fixed::~fixed()
+Fixed::~Fixed()
 {
+	cout << "Destructor called\n";
 }
 
-fixed&	fixed::operator=(const fixed& obj)
+Fixed&	Fixed::operator=(const Fixed& obj)
 {
+	cout << "Assignation operator called\n";
+	if (this != &obj)
+		value_ = obj.getRawBits();
+	return *this;
+}
+
+Fixed::Fixed(const int value): value_(value << kbits_)
+{
+	cout << "Int constructor called\n";
+}
+
+Fixed::Fixed(const float value): value_(roundf(value * (1 << kbits_)))
+{
+	cout << "Float constructor called\n";
+}
+
+int		Fixed::getRawBits(void) const
+{
+	cout << "getRawBits member function called\n";
+	return value_;
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	cout << "setRawBits member function called\n";
+	value_ = raw;
+}
+
+int		Fixed::toInt(void) const
+{
+	return value_ >> kbits_;
+}
+
+float	Fixed::toFloat(void) const
+{
+	return static_cast<float>(value_) / (1 << kbits_);
+}
+
+ostream&	operator<<(ostream& os, const Fixed& obj)
+{
+	os << obj.toFloat();
+	return os;
 }
--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -2,16 +2,34 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
-class fixed
+# include <iostream>
+# include <cmath>
+
+using std::cout;
+using std::roundf;
+using std::ostream;
+
+class Fixed
 {
-public:
-	fixed();
-	fixed(const fixed& obj);
-	~fixed();
-	fixed&	operator=(const fixed& obj);
+	public:
+		Fixed();
+		Fixed(const Fixed& obj);
+		~Fixed();
+		Fixed&	operator=(const Fixed& obj);
 
-private:
+		Fixed(int const value);
+		Fixed(float const value);
 
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;	
+
+	private:
+		static const int kbits_ = 8;
+		int value_;
 };
+
+ostream&	operator<<(ostream& os, const Fixed& obj);
 
 #endif
