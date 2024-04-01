451b8e96579527f39dc0b61d03251c81198e7f60
cozy_hn <jhk2721@gmail.com>
2024-04-01 21:40:47 +0900

    Update CPP02/ex03 : Refactor bsp.cpp

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -7,10 +7,6 @@ float cross(Point const p1, Point const p2)
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	float sign1 = cross(b - a, point - a) * cross(point - a, c - a);
-	float sign2 = cross(c - b, point - b) * cross(point - b, a - b);
-
-	if (sign1 > 0 && sign2 > 0)
-		return true;
-	return false;
+	return (cross(b - a, point - a) * cross(point - a, c - a) > 0 &&
+		cross(c - b, point - b) * cross(point - b, a - b) > 0);
 }
