b65931e33654e299342181bc8ba28ce3b122f495
cozy-hn <jhk2721@gmail.com>
2024-05-13 14:05:42 +0900

    Update cpp04 : update ex00 main

--- CPP04/ex00/main.cpp
+++ CPP04/ex00/main.cpp
@@ -1,5 +1,6 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
@@ -11,6 +12,21 @@ int main()
 	i->makeSound(); //will output the Cat sound!
 	j->makeSound();
 	meta->makeSound();
+
+	const WrongAnimal* meta2 = new WrongAnimal();
+	const WrongAnimal* j2 = new WrongCat();
+	const WrongCat* copy_j2 = static_cast<const WrongCat*>(j2);
+	std::cout << j2->getType() << " " << std::endl;
+	j2->makeSound();
+	meta2->makeSound();
+
+	delete meta;
+	delete j;
+	delete i;
+	delete meta2;
+	delete j2;
+	copy_j2->makeSound();
+	copy_j2->~WrongCat();
 
 	return 0;
 }
