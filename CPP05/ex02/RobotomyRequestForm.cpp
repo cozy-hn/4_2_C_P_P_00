f990e763c4291da1612bf8d3777f19758ce67862
cozy-hn <jhk2721@gmail.com>
2024-11-27 04:46:49 +0900

    feat: update cpp05/ex02

---
+++ CPP05/ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,32 @@
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string &target) : AForm("RobotomyRequestForm", target, 72, 45) {}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &other) : AForm(other) {}
+
+RobotomyRequestForm::~RobotomyRequestForm() {}
+
+RobotomyRequestForm& RobotomyRequestForm::operator=(const RobotomyRequestForm &other) {
+    if (this != &other) {
+        this->~RobotomyRequestForm();
+        new (this) RobotomyRequestForm(other);
+    }
+    return *this;
+}
+
+void RobotomyRequestForm::execute(const Bureaucrat &executor) const {
+    static bool first = true;
+    if (first) {
+        std::srand(std::time(0));
+        first = false;
+    }
+    if (!getIsSigned()) throw AForm::FormNotSignedException();
+    if (executor.getGrade() > getGradeToExecute()) throw AForm::GradeTooLowException();
+    std::cout << "* drilling noises *" << std::endl;
+    if (std::rand() % 2) {
+        std::cout << getTarget() << " has been robotomized successfully" << std::endl;
+    } else {
+        std::cout << "Robotomy failed" << std::endl;
+    }
+}
+
